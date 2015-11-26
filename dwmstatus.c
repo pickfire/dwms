9bdbf109555dca20fd50b1401fbd0faa1a551baf
Ivan Tham <pickfire@riseup.net>
2015-11-26 16:39:58 +0800

    Add volume and free space

--- dwmstatus.c
+++ dwmstatus.c
@@ -7,10 +7,14 @@
 #include <strings.h>
 #include <sys/time.h>
 #include <time.h>
+#include <sys/statvfs.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 #include <X11/Xlib.h>
+
+#include <alsa/asoundlib.h>
+#include <alsa/control.h>
 
 static Display *dpy;
 
@@ -103,14 +107,55 @@ runcmd(char *cmd)
 	return smprintf("%s", buf);
 }
 
+char *getfree(char *mnt){
+	struct statvfs buf;
+
+	if ( (statvfs(mnt, &buf)) < 0){
+		perror("can't get info on disk");
+		exit(1);
+	}
+	/* calculate the percentage of free/total */
+	return smprintf("%d%%", 100 * buf.f_bfree / buf.f_blocks);
+}
+
+/*
+int
+get_vol(void)
+{
+    int vol;
+    snd_hctl_t *hctl;
+    snd_ctl_elem_id_t *id;
+    snd_ctl_elem_value_t *control;
+
+// To find card and subdevice: /proc/asound/, aplay -L, amixer controls
+    snd_hctl_open(&hctl, "hw:0", 0);
+    snd_hctl_load(hctl);
+
+    snd_ctl_elem_id_alloca(&id);
+    snd_ctl_elem_id_set_interface(id, SND_CTL_ELEM_IFACE_MIXER);
+
+// amixer controls
+    snd_ctl_elem_id_set_name(id, "Master Playback Volume");
+
+    snd_hctl_elem_t *elem = snd_hctl_find_elem(hctl, id);
+
+    snd_ctl_elem_value_alloca(&control);
+    snd_ctl_elem_value_set_id(control, id);
+
+    snd_hctl_elem_read(elem, control);
+    vol = (int)snd_ctl_elem_value_get_integer(control,0);
+
+    snd_hctl_close(hctl);
+    return vol;
+}
+*/
+
 int
 main(void)
 {
-	char *status;
-	char *vol;
-	char *avgs;
-	char *tmtz;
 	unsigned int i;
+	char *tmtz, *avgs, *root, *vol = NULL;
+	char *status;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
@@ -120,14 +165,16 @@ main(void)
 	for (i = 0;;sleep(1), i++) {
 		if (i % 5 == 0) {
 			free(avgs);
+			free(root);
 			free(vol);
 			avgs = loadavg();
+			root = getfree("/");
 			vol = runcmd("amixer get PCM | grep -o '[0-9]*%'");
 		}
 		tmtz = mktimes("%a %d %b %T");
 
-		status = smprintf("♪:%s │ %s │ %s",
-				vol, avgs, tmtz);
+		status = smprintf("♪:%s │ / %s │ %s │ %s",
+				vol, root, avgs, tmtz);
 		setstatus(status);
 		free(tmtz);
 		free(status);
